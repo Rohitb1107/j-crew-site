a9f29154e0fb6064a9691651414c462db4ae91e7
Rohit <stylishrb711@gmail.com>
2022-06-18 21:26:54 +0530

    navbar update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,24 +1,55 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../Styles/navbar.css";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="nav-div">
       <ul>
+        <h3>J.CREW</h3>
         <li>
           <Link to="/">Home</Link>
+        </li>
+        <li>New</li>
+        <li>
+          <Link to="/womens">Womens</Link>
         </li>
         <li>
           <Link to="/mens">Mens</Link>
         </li>
         <li>
-          <Link to="/womens">Womens</Link>
-        </li>
-        <li>
           <Link to="/kids">Kids</Link>
         </li>
+        <li>Swim</li>
+        <li>Cashmere</li>
+        <li>Stories</li>
       </ul>
+      <div>
+        <img
+          className="iimg"
+          src="https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/search-512.png"
+          alt=""
+        />
+        <input type="text" placeholder="Search " className="search" />
+        <Link to="/Signin">
+          {" "}
+          <button id="sign"> Sign In </button>{" "}
+        </Link>
+
+        <img
+          className="heart"
+          src="https://www.iconpacks.net/icons/2/free-heart-icon-3510-thumb.png"
+          alt=""
+        />
+        <Link to="/Cart">
+          {" "}
+          <img
+            className="cartimg"
+            src="https://www.kindpng.com/picc/m/443-4431015_website-shopping-bag-icon-hd-png-download.png"
+            alt=""
+          />{" "}
+        </Link>
+      </div>
     </div>
   );
 };
